53fa81624ec41ebcb9be95261ee6d38132a88c5f
Domino <zhikhanastasya@gmail.com>
2018-10-22 16:31:06 +0300

    Add config settings for Ninject

--- IoC/ResolverConfig.cs
+++ IoC/ResolverConfig.cs
@@ -1,6 +1,12 @@
-﻿using BLL.Directory;
+﻿using BLL.Configuration;
+using BLL.Directory;
+using BLL.Directory.Logger;
+using BLL.Directory.MailGenerator;
 using BLL.Interfaces.Directoty;
+using BLL.Interfaces.Logger;
+using BLL.Interfaces.MailGenerator;
 using BLL.Interfaces.Services;
+using BLL.Interfaces.SetUp;
 using BLL.Services;
 using Ninject;
 
@@ -14,17 +20,26 @@ namespace IoC
         /// <param name="kernel">  Provides a path to register bindings. </param>
         public static void ConfigurateResolver(this IKernel kernel)
         {
-            // directory
-            kernel.Bind<IDirectoryWatcher>().To<DirectoryWatcher>();
+            // strategies
+            kernel.Bind<ISetUpManager>().To<SetUpManager>();
+
+            kernel.Bind<IMailGenerator<string>>().To<MailGenerator>();
+
+            kernel.Bind<IDirectoryWatcher>().To<DirectoryWatcher>()
+                .WithConstructorArgument("setUpManager", kernel.Get<ISetUpManager>());
+
+            kernel.Bind<ILogger>().To<Logger>();
 
             // services
-            kernel.Bind<IMailService<string>>().To<MailService>();
+            kernel.Bind<IMailService<string>>().To<MailService>()
+                .WithConstructorArgument("setUpManager", kernel.Get<ISetUpManager>())
+                .WithConstructorArgument("mailGenerator", kernel.Get<IMailGenerator<string>>())
+                .WithConstructorArgument("logger", kernel.Get<ILogger>());
 
             kernel.Bind<IDirectoryService>().To<DirectoryService>()
                 .WithConstructorArgument("watcher", kernel.Get<IDirectoryWatcher>())
-                .WithConstructorArgument("mailService", kernel.Get<IMailService<string>>());
-
-            // strategy
+                .WithConstructorArgument("mailService", kernel.Get<IMailService<string>>())
+                .WithConstructorArgument("setUpManager", kernel.Get<ISetUpManager>());
         }
     }
 }
