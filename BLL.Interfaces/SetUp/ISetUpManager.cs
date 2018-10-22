1e8e8a93d05ea80c77c08628799df9de606c9bbb
Domino <zhikhanastasya@gmail.com>
2018-10-22 11:56:12 +0300

    Add files

---
+++ BLL.Interfaces/SetUp/ISetUpManager.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.Interfaces.SetUp
+{
+    public interface ISetUpManager
+    {
+        string ReadSetting(string key);
+
+        void AddUpdateSetting(string key, string value);
+    }
+}
---
+++ BLL.Interfaces/SetUp/SetUpConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL.Interfaces.SetUp
+{
+    public static class SetUpConstants
+    {
+        public static readonly string SourcePath = "sourcePath";
+        public static readonly string SenderMailAddress = "senderAddress";
+        public static readonly string ReceiverMailAddress = "receiverAddress";
+        public static readonly string SenderMailPassword = "senderPassword";
+    }
+}
